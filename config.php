ae5f5cf41279ed68754700265973eaed89e9eb8e
root <root@YOGA-LENOVO.localdomain>
2022-02-16 18:16:37 +0300

    test

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php
-require_once realpath(__DIR__ . '/vendor/autoload.php');
+echo 1;
+require_once('vendor/autoload.php');
+echo 2;
 
 use Dotenv\Dotenv;
-
+echo 3;
 $dotenv = Dotenv::createImmutable(__DIR__);
+echo 4;
 $dotenv->load();
+echo 5;
 
 
 define('CURRCONV_KEY', $_ENV["CURRCONV_API_KEY"]);
